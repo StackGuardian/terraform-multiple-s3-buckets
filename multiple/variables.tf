98e772ef946b8a6309744be488242d80f9c0805e
Johannes Scheuerer <joscheuerer@gmail.com>
2024-06-06 21:03:42 +0200

    Create variables.tf

---
+++ multiple/variables.tf
@@ -0,0 +1,5 @@
+variable "buckets" {
+  type        = any
+  description = "List of maps containing for each bucket to create the info on 'bucket', 'bucket-region' and 'block_public_policy'"
+  default     = {}
+}
