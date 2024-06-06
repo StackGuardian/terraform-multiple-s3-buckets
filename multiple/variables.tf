7e83f1aa88becf6c9e95a1ff49c486bd5edf707e
Johannes Scheuerer <joscheuerer@gmail.com>
2024-06-06 22:19:37 +0200

    Update variables.tf

--- multiple/variables.tf
+++ multiple/variables.tf
@@ -3,3 +3,8 @@ variable "bucket-list" {
   description = "List of maps containing for each bucket to create the info on 'bucket', 'bucket-region' and 'block_public_policy'"
   default     = {}
 }
+variable "bucket_region" {
+  description = "Region where the bucket should be created"
+  type        = string
+  default     = ""
+}
