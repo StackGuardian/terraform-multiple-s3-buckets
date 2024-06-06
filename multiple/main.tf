77a7d84989e4c002ebbd7860067e3230a19909b2
Johannes Scheuerer <joscheuerer@gmail.com>
2024-06-06 22:35:42 +0200

    Update main.tf

--- multiple/main.tf
+++ multiple/main.tf
@@ -3,7 +3,6 @@ module "s3_bucket" {
   count = length(var.bucket-list)
 
   bucket = var.bucket-list[count.index].bucket
-  bucket_region = var.bucket-list[count.index].bucket_region
   block_public_policy = var.bucket-list[count.index].block_public_policy
   
   }
