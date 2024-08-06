064be9a4310d5968eed36f6521d482fc22a5b663
Johannes Scheuerer <joscheuerer@gmail.com>
2024-08-06 14:35:16 +0200

    Update main.tf

--- multiple/main.tf
+++ multiple/main.tf
@@ -1,3 +1,13 @@
+locals {
+
+bucket_list_with_id = [
+    for bucket in var.bucket_list:
+      merge(bucket, {id = index(bucket_list, bucket)+1})
+  ]
+
+}
+
+
 module "s3_bucket" {
   source = "../"
   count = length(var.bucket-list)
