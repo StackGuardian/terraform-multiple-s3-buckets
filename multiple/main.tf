ac033d7803641202b6c0a3a0ee80894154649040
Johannes Scheuerer <joscheuerer@gmail.com>
2024-08-07 12:15:47 +0200

    Update main.tf

--- multiple/main.tf
+++ multiple/main.tf
@@ -1,13 +1,18 @@
 locals {
-
-
 bucket-list-with-id = [
     for b in var.bucket-list:
-     #merge(b, {id = (data.terraform_remote_state.bucket-list[index(data.terraform_remote_state.bucket-list.bucket, b.bucket.value)] != null? data.terraform_remote_state.bucket-list.id : index(var.bucket-list, bucket)+1)})
-      merge(b, {id = (previous-bucket-list[index(previous-bucket-list.bucket, b.bucket.value)] != null? previous-bucket-list.id : index(var.bucket-list, bucket)+1)})
+     merge(b, {id = (data.terraform_remote_state.state.outputs.bucket-list[index(data.terraform_remote_state.state.outputs.bucket-list.bucket, b.bucket.value)] != null? data.terraform_remote_state.state.outputs.bucket-list.id : index(var.bucket-list, bucket)+1)})
+    #merge(b, {id = (previous-bucket-list[index(previous-bucket-list.bucket, b.bucket.value)] != null? previous-bucket-list.id : index(var.bucket-list, bucket)+1)})
   ]
 }
+module "backend_config" {
+  source = "Invicton-Labs/backend-config/null"
+}
 
+data "terraform_remote_state" "state" {
+  backend   = module.backend_config.backend.type
+  config    = module.backend_config.backend.config
+}
 
 module "s3_bucket" {
   source = "../"
