af37023259d91986ff0f23dad4596539bfa03192
Johannes Scheuerer <joscheuerer@gmail.com>
2024-08-07 21:32:40 +0200

    Update main.tf

--- multiple/main.tf
+++ multiple/main.tf
@@ -1,8 +1,8 @@
 locals {
   bucket-list-with-id = [
     for b in var.bucket-list:
-     #merge(b, {id = index(var.bucket-list, b)+1})
-     merge(b, {id = (data.terraform_remote_state.state[0].outputs.bucket-list[index(data.terraform_remote_state.state[0].outputs.bucket-list.bucket, b.bucket.value)] != null? data.terraform_remote_state.state[0].outputs.bucket-list.id : index(var.bucket-list, b)+1)})
+     merge(b, {id = index(var.bucket-list, b)+1})
+     #merge(b, {id = (data.terraform_remote_state.state[0].outputs.bucket-list[index(data.terraform_remote_state.state[0].outputs.bucket-list.*.bucket, b.bucket)] != null? data.terraform_remote_state.state[0].outputs.bucket-list.id : index(var.bucket-list, b)+1)})
  
   ]
 }
@@ -27,5 +27,6 @@ module "s3_bucket" {
 
   bucket = local.bucket-list-with-id[count.index].bucket
   block_public_policy = local.bucket-list-with-id[count.index].block_public_policy
+  tags = {ID = local.bucket-list-with-id[count.index].id}
   
   }
